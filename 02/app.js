aa356e6180397548ebeda23359952bf04593f256
Kimkyeongbeom4844 <bibix321@naver.com>
2022-03-10 17:54:48 +0900

    02

--- 02/app.js
+++ 02/app.js
@@ -2,5 +2,12 @@ let names = 'kkb';
 names = 'Kimkyeongbeom';
 console.log(names);
 
+let todos = ['운동'];
+let todo = '게임';
+todos.push(todo);
+console.log(todos); //['운동','게임']
+
+
+
 // 변수명을 name으로하면 오류가 뜬다 -> window.name이 이미 존재
 // window.name은 현재 창의 이름을 나타냄 
