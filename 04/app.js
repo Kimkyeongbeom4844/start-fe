d424e03fb69cc9b48d89bfa886cbf14368b831f4
Kimkyeongbeom4844 <bibix321@naver.com>
2022-03-31 14:00:44 +0900

    퀴즈 1,2 추가
    개씹하드코딩...

---
+++ 04/app.js
@@ -0,0 +1,9 @@
+// console.log('4장');
+// const elements = document.getElementById('wrap');
+// console.log(element);
+// const item = document.querySelector('.item');
+// console.log(item)
+
+window.onload = function(){
+    console.log("콜백hello");
+}
